523093bc31bd8cdf8af1a44d3fa942c2e113af7d
quangvuong2541 <truongquangvuong2000@gmail.com>
2022-09-13 22:08:50 +0700

    routerredux

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// redux thunk
+import { Provider } from 'react-redux';
+import thunk from "redux-thunk"
+import { applyMiddleware, createStore, compose } from "redux"
+import { rootReducer } from './redux/rootReducer';
+// copy paste 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  // để dành khi học kết nối với api thì sẽ giải thích middleware là gì
+  composeEnhancers(applyMiddleware(thunk))
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
